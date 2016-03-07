b886bb4cd9df4edbcec56f488256cb6d18a91946
venkateshguntuka <venkateshguntuka@missionrnd.com>
2016-03-07 19:57:02 +0530

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -20,7 +20,34 @@ struct student {
 	char *name;
 	int score;
 };
-
-struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+void sortStructure(struct student *students, int len);
+struct student ** topKStudents(struct student *students, int len, int K)
+{
+	if (len < 1 || K <= 0)
+		return NULL;
+	if (K > len)
+		K = len;
+	struct student **result = (struct student **)malloc(K*sizeof(struct student));
+	sortStructure(&students[0], len);
+	for (int i = 0; i < K; i++)
+	{
+		result[i] = &students[i];
+	}
+	return result;
+}
+void sortStructure(struct student *students, int len)
+{
+	struct student temp;
+	for (int i = 0; i <len; i++)
+	{
+		for (int j = i + 1; j < len; j++)
+		{
+			if (students[i].score < students[j].score)
+			{
+				temp = students[i];
+				students[i] = students[j];
+				students[j] = temp;
+			}
+		}
+	}
 }
